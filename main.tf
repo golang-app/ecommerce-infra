b32c85a971cffa39b9401ba34307874ea257d758
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2023-04-06 20:56:18 +0200

    initial

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.9.0"
+
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "observability" {
+  metadata {
+    name = "observability"
+  }
+}
+
+resource "helm_release" "otel-collector"{
+  name       = "otel-collector"
+
+  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart      = "opentelemetry-collector"
+
+  set {
+    name  = "mode"
+    value = "deployment"
+  }
+  
+    namespace = kubernetes_namespace.observability.metadata[0].name
+}
