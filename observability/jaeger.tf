40cd4bd68fa54bd33e9ebb6e84d8addba72fef6c
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2023-04-07 12:16:19 +0200

    fmt

--- observability/jaeger.tf
+++ observability/jaeger.tf
@@ -4,7 +4,7 @@ resource "helm_release" "jaeger_operator" {
   chart      = "jaeger-operator"
   version    = var.jaeger_operator_chart_version
   timeout    = 3600
-# Ensure jaeger-operator operates on things (e.g. CRs) in cluster-wide scope.
+  # Ensure jaeger-operator operates on things (e.g. CRs) in cluster-wide scope.
   set {
     name  = "rbac.clusterRole"
     value = true
