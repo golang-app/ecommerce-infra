2b23e86c3b57c8ddfc01080880783ad46898059a
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2023-04-08 20:26:25 +0200

    add grafana

--- observability/jaeger.tf
+++ observability/jaeger.tf
@@ -3,6 +3,7 @@ resource "helm_release" "jaeger_operator" {
   name       = "jaeger-operator"
   chart      = "jaeger-operator"
   version    = var.jaeger_operator_chart_version
+  namespace  = var.namespace
   timeout    = 3600
   # Ensure jaeger-operator operates on things (e.g. CRs) in cluster-wide scope.
   set {
